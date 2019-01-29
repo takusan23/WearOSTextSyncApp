eb58bec08067c3b858f76f712bdbbdcd3455d13f
takusan23 <takumu1200@gmail.com>
2019-01-29 23:17:40 +0900

    圧倒的感謝

--- app/src/main/java/io/github/takusan23/testwearosapp/MainActivity.java
+++ app/src/main/java/io/github/takusan23/testwearosapp/MainActivity.java
@@ -23,7 +23,7 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-    String datapath = "/message_path";
+    String datapath = "/message";
     int num = 1;
 
     Button sendButton;
@@ -42,84 +42,39 @@ public class MainActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message = editText.getText().toString();
-                //Requires a new thread to avoid blocking the UI
-                new SendThread(datapath, message).start();
+                final String message = editText.getText().toString();
                 //テキストビュー
                 textView.append(editText.getText().toString() + "\n");
-                //num++;
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Node(接続先？)検索
+                        Task<List<Node>> nodeListTask =
+                                Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+                        try {
+                            List<Node> nodes = Tasks.await(nodeListTask);
+                            for (Node node : nodes) {
+                                //sendMessage var1 は名前
+                                //sendMessage var2 はメッセージ
+                                Task<Integer> sendMessageTask =
+                                        Wearable.getMessageClient(MainActivity.this).sendMessage(node.getId(), datapath, message.getBytes());
+
+                                Integer result = Tasks.await(sendMessageTask);
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }).start();
+
+
             }
         });
 
-    }
-
-/*
-    public void sendmessage(String logthis) {
-        Bundle b = new Bundle();
-        b.putString("logthis", logthis);
-        Message msg = handler.obtainMessage();
-        msg.setData(b);
-        msg.arg1 = 1;
-        msg.what = 1; //so the empty message is not used!
-        handler.sendMessage(msg);
-
-    }
-*/
-
-    //This actually sends the message to the wearable device.
-    class SendThread extends Thread {
-        String path;
-        String message;
-
-        //constructor
-        SendThread(String p, String msg) {
-            path = p;
-            message = msg;
-        }
-
-        //sends the message via the thread.  this will send to all wearables connected, but
-        //since there is (should only?) be one, no problem.
-        public void run() {
-
-            //first get all the nodes, ie connected wearable devices.
-            Task<List<Node>> nodeListTask =
-                    Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
-            try {
-                // Block on a task and get the result synchronously (because this is on a background
-                // thread).
-                List<Node> nodes = Tasks.await(nodeListTask);
-
-                //Now send the message to each device.
-                for (Node node : nodes) {
-                    Task<Integer> sendMessageTask =
-                            Wearable.getMessageClient(MainActivity.this).sendMessage(node.getId(), path, message.getBytes());
-
-                    try {
-                        // Block on a task and get the result synchronously (because this is on a background
-                        // thread).
-                        Integer result = Tasks.await(sendMessageTask);
-                        //sendmessage("SendThread: message send to " + node.getDisplayName());
-                        //Log.v(TAG, "SendThread: message send to " + node.getDisplayName());
-
-                    } catch (ExecutionException exception) {
-                        //sendmessage("SendThread: message failed to" + node.getDisplayName());
-                        //Log.e(TAG, "Send Task failed: " + exception);
-
-                    } catch (InterruptedException exception) {
-                        //Log.e(TAG, "Send Interrupt occurred: " + exception);
-                    }
-
-                }
-
-            } catch (ExecutionException exception) {
-                //sendmessage("Node Task failed: " + exception);
-                //Log.e(TAG, "Node Task failed: " + exception);
-
-            } catch (InterruptedException exception) {
-                //Log.e(TAG, "Node Interrupt occurred: " + exception);
-            }
-
-        }
     }
 }
 
